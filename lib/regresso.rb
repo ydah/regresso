3726688295f75fb6e3ce54cede812017c49bf5c1
ydah <t.yudai92@gmail.com>
2026-01-18 14:38:25 +0900

    Initial commit

---
+++ lib/regresso.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "regresso/version"
+
+module Regresso
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/regresso/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Regresso
+  VERSION = "0.1.0"
+end
